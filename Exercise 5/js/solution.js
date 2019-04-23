89def7c79466e202a7978b28426b9f946cf2040e
Andrew Chuah <43582942+Sheathbladetrox@users.noreply.github.com>
2019-04-22 22:22:26 -0400

    Add files via upload

---
+++ Exercise 5/js/solution.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function(){
+    var featured = document.querySelector("#featured>img")
+    var images = document.addEventListener("#thumbnails img")
+
+    images.addEventListener("click", function(){
+        
+    }
+
+}
